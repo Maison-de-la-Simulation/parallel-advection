fa5635ee62ca5111893ff5b5f05369d4e3958ba1
Aympab <aymeric.millan@cea.fr>
2023-04-17 20:48:34 +0200

    scoped CPU ok

--- src/core/impl/Scoped.cpp
+++ src/core/impl/Scoped.cpp
@@ -11,8 +11,8 @@ AdvX::Scoped::operator()(sycl::queue &Q, sycl::buffer<double, 2> &buff_fdistrib,
 
 //   const sycl::range<2> nb_wg{1, nVx/4};
 //   const sycl::range<2> wg_size{nx, 4};
-  const sycl::range<2> nb_wg{2, nVx};
-  const sycl::range<2> wg_size{nx/2, 1};
+  const sycl::range<2> nb_wg{1, nVx};
+  const sycl::range<2> wg_size{nx, 1};
 
   return Q.submit([&](sycl::handler &cgh) {
     auto fdist = buff_fdistrib.get_access<sycl::access::mode::read_write>(cgh);
@@ -21,10 +21,8 @@ AdvX::Scoped::operator()(sycl::queue &Q, sycl::buffer<double, 2> &buff_fdistrib,
 
     // cgh.copy()
     cgh.parallel(nb_wg, wg_size, [=](auto g) {
-      // c.f.
-      // https://github.com/OpenSYCL/OpenSYCL/blob/develop/doc/scoped-parallelism.md#memory-placement-rules
-    //   double slice_ftmp[nx];   // declared in the private memory of the
-                               // executing physical WI ???
+      // c.f. https://github.com/OpenSYCL/OpenSYCL/blob/develop/doc/scoped-parallelism.md#memory-placement-rules
+    //   double slice_ftmp[nx];   // declared in the private memory of the executing physical WI ???
         //Actually doesn't work if version of CUDA is not 11.6. I have to use the local_accessor
 
 
