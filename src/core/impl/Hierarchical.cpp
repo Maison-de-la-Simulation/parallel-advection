b9d06ac681ba6e535d2344a1e374ef43358e3aa1
Aympab <aymeric.millan@cea.fr>
2023-05-03 11:36:59 +0200

    Update hierarchical to have a work group copy

--- src/core/impl/Hierarchical.cpp
+++ src/core/impl/Hierarchical.cpp
@@ -12,7 +12,7 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
 
     // assert(nVx % 512 == 0);
     const sycl::range<2> nb_wg{nVx, 1};
-    const sycl::range<2> wg_size{1, 512};
+    const sycl::range<2> wg_size{1, 1024};
 
     return Q.submit([&](sycl::handler &cgh) {
         auto fdist =
@@ -21,10 +21,17 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
         sycl::local_accessor<double, 1> slice_ftmp(sycl::range<1>{nx}, cgh);
         // double slice_ftmp[512];vi
         cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<2> g) {
+
+            const int ivx = g.get_group_id(0);
+            g.async_work_group_copy(slice_ftmp.get_pointer(),
+                                    fdist.get_pointer() + ivx*nx,
+                                    nx).wait();
+
             g.parallel_for_work_item(
                 sycl::range<2>(1, nx), [&](sycl::h_item<2> it) {
                     const int ix = it.get_global_id(1);
-                    const int ivx = g.get_group_id(0);
+                    //g.get_group_id(0) also works for ivx
+                    const int ivx = it.get_global_id(0);
 
                     double const xFootCoord = displ(ix, ivx, params);
 
@@ -43,22 +50,14 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
                     const int ipos1 = LeftDiscreteNode - LAG_OFFSET;
 
                     // double ftmp = 0.;
-                    slice_ftmp[ix] = 0;
+                    // slice_ftmp[ix] = 0;
+                    fdist[ivx][ix] = 0;
                     for (int k = 0; k <= LAG_ORDER; k++) {
                         int idx_ipos1 = (nx + ipos1 + k) % nx;
 
-                        slice_ftmp[ix] += coef[k] * fdist[ivx][idx_ipos1];
+                        fdist[ivx][ix] += coef[k] * slice_ftmp[idx_ipos1];
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
-
-            g.parallel_for_work_item(sycl::range<2>(1, nx),
-                                     [&](sycl::h_item<2> it) {
-                                         const int ix = it.get_global_id(1);
-                                         const int ivx = it.get_global_id(0);
-                                         fdist[ivx][ix] = slice_ftmp[ix];
-                                     });
-
-            // g.async_work_group_copy();
 
         });   // end parallel_for_work_group
     });       // end Q.submit
