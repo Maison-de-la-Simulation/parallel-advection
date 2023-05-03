69c815955872c41909df56abf9539fbb27a6869a
Aympab <aymeric.millan@cea.fr>
2023-05-03 11:44:41 +0200

    update hierarchical

--- src/core/impl/Hierarchical.cpp
+++ src/core/impl/Hierarchical.cpp
@@ -21,6 +21,12 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
         sycl::local_accessor<double, 1> slice_ftmp(sycl::range<1>{nx}, cgh);
         // double slice_ftmp[512];vi
         cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<2> g) {
+
+            const int ivx = g.get_group_id(0);
+            g.async_work_group_copy(slice_ftmp.get_pointer(),
+                                    fdist.get_pointer() + ivx * nx,
+                                    nx).wait();
+
             g.parallel_for_work_item(
                 sycl::range<2>(1, nx), [&](sycl::h_item<2> it) {
                     const int ix = it.get_global_id(1);
@@ -43,21 +49,13 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
 
                     const int ipos1 = LeftDiscreteNode - LAG_OFFSET;
 
-                    // double ftmp = 0.;
-                    // fdist[ivx][ix] = 0;
-                    slice_ftmp[ix] = 0;
+                    fdist[ivx][ix] = 0;
                     for (int k = 0; k <= LAG_ORDER; k++) {
                         int idx_ipos1 = (nx + ipos1 + k) % nx;
 
-                        // fdist[ivx][ix] += coef[k] * slice_ftmp[idx_ipos1];
-                        slice_ftmp[ix] += coef[k] * fdist[ivx][idx_ipos1];
+                        fdist[ivx][ix] += coef[k] * slice_ftmp[idx_ipos1];
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
-
-            const int ivx = g.get_group_id(0);
-            g.async_work_group_copy(fdist.get_pointer() + ivx * nx,
-                                    slice_ftmp.get_pointer(), nx).wait();
-                                    
         });   // end parallel_for_work_group
     });       // end Q.submit
 }
