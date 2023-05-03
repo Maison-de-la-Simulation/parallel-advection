97fda8c9bad2bfbcb232a416b8c76d43ab72215e
Aympab <aymeric.millan@cea.fr>
2023-05-03 11:55:59 +0200

    trying malloc_device

--- src/core/impl/Hierarchical.cpp
+++ src/core/impl/Hierarchical.cpp
@@ -14,18 +14,26 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
     const sycl::range<2> nb_wg{nVx, 1};
     const sycl::range<2> wg_size{1, 1024};
 
+    auto buff_ftmp = sycl::malloc_device<double>(nx*sizeof(double)*nVx, Q);
     return Q.submit([&](sycl::handler &cgh) {
         auto fdist =
             buff_fdistrib.get_access<sycl::access::mode::read_write>(cgh);
 
-        sycl::local_accessor<double, 1> slice_ftmp(sycl::range<1>{nx}, cgh);
+        // sycl::local_accessor<double, 1> slice_ftmp(sycl::range<1>{nx}, cgh);
         // double slice_ftmp[512];vi
         cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<2> g) {
 
-            const int ivx = g.get_group_id(0);
-            g.async_work_group_copy(slice_ftmp.get_pointer(),
-                                    fdist.get_pointer() + ivx * nx,
-                                    nx).wait();
+            // const int ivx = g.get_group_id(0);
+            // g.async_work_group_copy(slice_ftmp.get_pointer(),
+            //                         fdist.get_pointer() + ivx * nx,
+            //                         nx).wait();
+            g.parallel_for_work_item(
+                sycl::range(1, nx), [&](sycl::h_item<2> item) {
+                    const int ix = item.get_global_id(1);
+                    const int ivx = item.get_global_id(0);
+
+                    buff_ftmp[ix + nx*ivx] = fdist[ivx][ix];
+           });
 
             g.parallel_for_work_item(
                 sycl::range<2>(1, nx), [&](sycl::h_item<2> it) {
@@ -53,7 +61,8 @@ AdvX::Hierarchical::operator()(sycl::queue &Q,
                     for (int k = 0; k <= LAG_ORDER; k++) {
                         int idx_ipos1 = (nx + ipos1 + k) % nx;
 
-                        fdist[ivx][ix] += coef[k] * slice_ftmp[idx_ipos1];
+                        // fdist[ivx][ix] += coef[k] * slice_ftmp[idx_ipos1];
+                        fdist[ivx][ix] += coef[k] * buff_ftmp[idx_ipos1 + nx*ivx];
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
         });   // end parallel_for_work_group
