95001110f83e4c3ae2d4499d066d8f1eaa22289e
Aympab <aympab@gmail.com>
2023-06-26 14:02:25 +0200

    update cmake

--- src/sycl/main.cpp
+++ src/sycl/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <sycl/sycl.hpp>
 #include <validation.h>
+#include <filesystem>
 
 // ==========================================
 // ==========================================
