ccc62d70558128471e3cddd0d6aabcba2842a35b
Aympab <aympab@gmail.com>
2023-11-15 19:12:05 +0100

    cleaning repo

--- containers/sycl-full/scripts/install_rocm.5.4.3.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y libnuma-dev cmake unzip
-wget --progress=bar:force -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
-echo "deb [arch=amd64] https://repo.radeon.com/rocm/apt/5.4.3 focal main" | sudo tee /etc/apt/sources.list.d/rocm.list
-printf 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/rocm-pin-600
-sudo apt update -y
-sudo apt install -y rocm-dev
