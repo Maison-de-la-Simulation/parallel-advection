303263a5311a97c68ada37f1851acbf67f293b4b
Aympab <aympab@gmail.com>
2024-01-21 13:46:44 +0100

    update slurm submission

--- diag/launch_benchmarks.sh
+++ diag/launch_benchmarks.sh
@@ -1,51 +1,72 @@
 #!/bin/bash 
-#SBATCH --job-name=SYCLADV
+#SBATCH --job-name=SYCL_Adv
+#SBATCH --time=05:00:00
 #SBATCH --output=%x.o%j
 #SBATCH --nodes=1
 #SBATCH --exclusive
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpua100
-#SBATCH --time=10:00:00
 
 #============================================================
-#Change this depending on the cluster
-export HOME_FOLDER=/gpfs/users/millana
+# RUCHE
+#============================================================
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=gpua100
+
+###SBATCH --cpus-per-task=40
+###SBATCH --partition cpu_long
+
+#============================================================
+# ADASTRA
+#============================================================
+# export SINGULARITY_BIND="/proc,/sys,/dev"
+
+###SBATCH --account=cin4492
+###SBATCH --gres=gpu:1
+###SBATCH --gpus-per-node=1
+###SBATCH --cpus-per-task=64
+###SBATCH --constraint=MI250
+
+###SBATCH --account=cin4691
+###SBATCH --cpus-per-task=192
+###SBATCH --constraint=GENOA
+
+#============================================================
 export CONTAINER_RUN=$CONTAINERSDIR/sycl-complete_latest.sif
-export OUTFILE=$HOME_FOLDER/a100_acpp.json
-export BENCH_EXEC=/gpfs/users/millana/source/parallel-advection/build_acpp/benchmark/main-bench
+
+export OUTFILE_DIR=/gpfs/users/my_user
+export BUILD_DIR=/gpfs/users/my_user/parallel-advection/build_acpp
+
+export HW=a100
+export IMPL=acpp
+export ARGS_GPU_SINGULARITY= #--rocm #--nv
+
+#Benchmark parameters
+export BENCH_EXEC=${BUILD_DIR}/benchmark/main-bench
+export BENCHMARK_FILTER=BM_Advector/0
+export BENCHMARK_REPETITIONS=10
+
+export OUTFILE=$OUTFILE_DIR/${HW}_${IMPL}_${BENCHMARK_REPETITIONS}rep.json
 #============================================================
 
 #============================================================
 #Modules
 module purge
-. ~/singularity-env.sh
-# export SINGULARITY_BIND="$HOME_FOLDER:/mnt,/proc,/sys,/dev"
+if [ -e "~/singularity-env.sh" ]; then
+    . ~/singularity-env.sh
+fi
+export SINGULARITY_BIND="$SINGULARITY_BIND,$OUTFILE_DIR,$BUILD_DIR"
 #============================================================
 
 export COMMAND="$BENCH_EXEC \
---benchmark_filter=BM_Advector \
 --benchmark_counters_tabular=true \
---benchmark_repetitions=10 \
 --benchmark_report_aggregates_only=true \
 --benchmark_min_warmup_time=1 \
 --benchmark_format=json \
 --benchmark_out=$OUTFILE"
 
 singularity exec \
---env OMP_NUM_THREADS=32 \
+--env OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK} \
 --env OMP_PLACES=cores \
---nv \
+--env OMP_PROC_BIND=true \
+$ARGS_GPU_SINGULARITY \
 $CONTAINER_RUN $COMMAND
-
-
-# # # QUEUE SUBMISSION BENCHMARK
-# # export COMMAND="/gpfs/users/millana/source/parallel-advection/build_acpp/benchmark/submission-bench \
-# # --benchmark_counters_tabular=true \
-# # --benchmark_out=$HOME_FOLDER/a100_submission.json"
-
-# # singularity exec \
-# # --env OMP_NUM_THREADS=32 \
-# # --env OMP_PLACES=cores \
-# # --env OMP_PLACES=cores \
-# # --nv \
-# # $CONTAINER_RUN $COMMAND
