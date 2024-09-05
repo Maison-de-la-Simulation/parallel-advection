9ff90175e8d78128053a22d027f57d07d48a9e2b
Aympab <aymeric.millan@cea.fr>
2024-09-05 16:32:13 +0200

    Add straddled malloc to Exp1

--- src/core/impl/Exp1.cpp
+++ src/core/impl/Exp1.cpp
@@ -4,8 +4,20 @@
 // this is streamY + straddlel malloc + twoDimwg
 //  TODO: implement percent_in_global_mem values
 
-constexpr size_t MAX_NX_ALLOC = 6144;   // A100
+// constexpr size_t MAX_NX_ALLOC = 6144;   // A100
+constexpr size_t MAX_NX_ALLOC = 64;
 constexpr size_t MAX_NY = 128;
+
+struct StraddledBuffer {
+    // sycl::buffer
+    // sycl::local_accessor
+
+    // ctor(percentage alloc, ny, nx, ny1, cgh_for_local_accessor)
+
+    // index_limit
+    //  operator[i] doing the if i > index_limit return global accessor else
+    //  return local_accessor
+};
 
 // ==========================================
 // ==========================================
@@ -29,39 +41,67 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
 
     /* ny must be divisible by slice_size_dim_y */
     if (ny_batch_size % wg_size_y != 0) {
-        throw std::invalid_argument("ny_batch_size must be divisible by wg_size_y");
-    }
-    if (wg_size_y * nx > 6144) {
-        std::cout << "wg_size_y = " << wg_size_y << ", nx = " << nx
-                  << std::endl;
         throw std::invalid_argument(
-            "wg_size_y*nx must be < to 6144 (shared memory limit)");
+            "ny_batch_size must be divisible by wg_size_y");
     }
+    // if (wg_size_y * nx > 6144) {
+    //     std::cout << "wg_size_y = " << wg_size_y << ", nx = " << nx
+    //               << std::endl;
+    //     throw std::invalid_argument(
+    //         "wg_size_y*nx must be < to 6144 (shared memory limit)");
+    // }
 
     const sycl::range nb_wg{ny_batch_size / wg_size_y, 1, ny1};
     const sycl::range wg_size{params.wg_size_y, params.wg_size_x, 1};
 
-    // std::cout << "flag1" << std::endl;
-    // /* Straddled malloc*/
-    // std::unique_ptr<sycl::buffer<double, 2>> ptr_buff_rest_nx;
-    // // auto buff_rest_nx = *ptr_buff_rest_nx;
-    // std::cout << "flag2" << std::endl;
-    // if (nx_rest_to_malloc > 0)
-    //     *ptr_buff_rest_nx = sycl::buffer<double, 2>(sycl::range{ny,
-    //     nx_rest_to_malloc}, sycl::no_init);
+    sycl::buffer<double, 2> buff_rest_nx(sycl::range{ny, nx_rest_to_malloc},
+                                         sycl::no_init);
 
-    // std::cout << "flag3" << std::endl;
+    /*What about two kernels the first one fills the overslice as write only
+    memory and the second one fills the local accessor and solves the advection?
+    so we can use read only and write only
+    */
+    // Q.submit([&](sycl::handler &cgh) {
+    //     auto fdist =
+    //         buff_fdistrib.get_access<sycl::access::mode::read_write>(cgh);
+
+    // }).wait();
+
     return Q.submit([&](sycl::handler &cgh) {
         auto fdist =
             buff_fdistrib.get_access<sycl::access::mode::read_write>(cgh);
 
+        auto overslice_ftmp =
+            buff_rest_nx.get_access<sycl::access::mode::discard_read_write>(
+                cgh);
+
         /* We use a 2D local accessor here */
+        auto local_malloc_size = nx > MAX_NX_ALLOC ? MAX_NX_ALLOC : nx;
         sycl::local_accessor<double, 2> slice_ftmp(
-            sycl::range<2>(wg_size_y, nx), cgh);
+            sycl::range<2>(wg_size_y, local_malloc_size), cgh, sycl::no_init);
 
         cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<3> g) {
             // if y1 > 1 //if we have a stide, we transpose, else we copy
 
+            /* Copy kernel*/
+            g.parallel_for_work_item(
+                sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
+                    const int ix = it.get_local_id(1);
+                    const int iz = g.get_group_id(2);
+
+                    const int local_ny = it.get_local_id(0);
+                    const int ivx =
+                        wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
+
+                    if (ix < MAX_NX_ALLOC) {
+                        slice_ftmp[local_ny][ix] = fdist[ivx][ix][iz];
+                    } else {
+                        overslice_ftmp[ivx][ix - MAX_NX_ALLOC] =
+                            fdist[ivx][ix][iz];
+                    }
+                });   // barrier
+
+            /* Solve kernel */
             g.parallel_for_work_item(
                 sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
                     const int ix = it.get_local_id(1);
@@ -85,28 +125,25 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
 
                     const int ipos1 = leftNode - LAG_OFFSET;
 
-                    slice_ftmp[local_ny][ix] = 0.;
+                    fdist[ivx][ix][iz] = 0.;
                     for (int k = 0; k <= LAG_ORDER; k++) {
                         int idx_ipos1 = (nx + ipos1 + k) % nx;
 
-                        slice_ftmp[local_ny][ix] +=
-                            coef[k] * fdist[ivx][idx_ipos1][iz];
+                        if (idx_ipos1 < MAX_NX_ALLOC) {
+                            fdist[ivx][ix][iz] +=
+                                coef[k] * slice_ftmp[local_ny][idx_ipos1];
+                        } else {
+                            fdist[ivx][ix][iz] +=
+                                coef[k] *
+                                overslice_ftmp[ivx][idx_ipos1 - MAX_NX_ALLOC];
+                        }
+
+                        // fdist[ivx][ix][iz] +=
+                        //     coef[k] * slice_ftmp[local_ny][idx_ipos1];
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
-
-            g.parallel_for_work_item(
-                sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
-                    const int ix = it.get_local_id(1);
-                    const int iz = g.get_group_id(2);
-
-                    const int local_ny = it.get_local_id(0);
-                    const int ivx =
-                        wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
-
-                    fdist[ivx][ix][iz] = slice_ftmp[local_ny][ix];
-                });
-        });   // end parallel_for_work_group
-    });       // end Q.submit
+        });           // end parallel_for_work_group
+    });               // end Q.submit
 }   // end actual_advection
 
 // ==========================================
