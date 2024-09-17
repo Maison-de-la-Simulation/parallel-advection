8f22a82f8d2d568d6b6c87193917394c040d560f
Aympab <aymeric.millan@cea.fr>
2024-09-17 10:37:08 +0200

    Update straddled struct

--- src/core/impl/Exp1.cpp
+++ src/core/impl/Exp1.cpp
@@ -5,75 +5,53 @@
 #include <memory>
 #include <type_traits>
 
-using mdspan_t =
-    std::experimental::mdspan<double, std::experimental::dextents<size_t, 3>,
+using real_t = double;
+
+using mdspan3d_t =
+    std::experimental::mdspan<real_t, std::experimental::dextents<size_t, 3>,
+                              std::experimental::layout_right>;
+using mdspan2d_t =
+    std::experimental::mdspan<real_t, std::experimental::dextents<size_t, 2>,
                               std::experimental::layout_right>;
 
-// this is streamY + straddlel malloc + twoDimwg
-//  TODO: implement percent_in_global_mem values
+using globAcc2D =
+    typename sycl::accessor<real_t, 2,
+                            sycl::access::mode::discard_read_write,
+                            sycl::target::global_buffer>;
 
-// constexpr size_t MAX_NX_ALLOC = 6144;   // A100
-constexpr size_t MAX_NX_ALLOC = 64;
-constexpr size_t MAX_NY = 128;
+using localAcc2D = typename sycl::local_accessor<real_t, 2>;
+//TODO: implement percent_in_global_mem values
 
-template<typename RealType>
-struct StraddledBuffer {
-    template<typename Tp>
-    using ptr_t = std::shared_ptr<Tp>;
+//constexpr size_t MAX_NX_ALLOC = 6144;   // A100
 
-    using globAcc2D =
-        typename sycl::accessor<RealType, 2,
-                                sycl::access::mode::discard_read_write,
-                                sycl::target::global_buffer>;
-    using localAcc2D = typename sycl::local_accessor<RealType, 2>;
+constexpr size_t MAX_NX_ALLOC = 6144;
+constexpr size_t MAX_NY = 65535;
 
-    ptr_t<globAcc2D> m_global_acc;
-    ptr_t<localAcc2D> m_local_acc;
-    size_t localNy, localNx; //TODO: careful if localNx is different than MAX_NX_ALLOC, we might want that ? For now we stay simple with only localNx == MAX_NX_ALLOC
-    size_t globalNy, globalNx; //size of local buffer
+template <typename RealType> struct StraddledBuffer {
+
+    mdspan2d_t m_local_mdpsan;
+    mdspan2d_t m_global_mdpsan;
 
     StraddledBuffer() = delete;
     StraddledBuffer(const globAcc2D &globalAcc, const localAcc2D &localAcc)
-        : globalNy(globalAcc.get_range().get(0)),
-          globalNx(globalAcc.get_range().get(1)),
-          localNy(localAcc.get_range().get(0)),
-          localNx(localAcc.get_range().get(1)) {
-        m_global_acc = std::make_unique<globAcc2D>(globalAcc);
-        m_local_acc  = std::make_unique<localAcc2D>(localAcc);
+        : m_local_mdpsan(localAcc.get_pointer(), localAcc.get_range().get(0),
+                         localAcc.get_range().get(1)),
+          m_global_mdpsan(globalAcc.get_pointer(), globalAcc.get_range().get(0),
+                          globalAcc.get_range().get(1)) {}
+
+    RealType &operator()(size_t iy, size_t ix) const {
+        auto localNx = m_local_mdpsan.extent(1);
+        auto localNy = m_local_mdpsan.extent(0);
+
+        if (ix < localNx){
+            return m_local_mdpsan(iy % localNy, ix);
+        }
+        else{
+            return m_global_mdpsan(iy, ix - localNx);
+        }
     }
-
-    RealType& operator()(size_t iy, size_t ix) {
-        if(ix < localNx)
-            // return localMdspan()(iy % localNy, ix);
-            return (*m_local_acc)[iy % localNy][ix];
-        else
-            // return globalMdspan()(iy, ix - localNx);
-            return (*m_global_acc)[iy][ix - localNx];
-    }
-    
-    const RealType operator()(size_t iy, size_t ix) const{
-        if(ix < localNx)
-            return localMdspan()(iy % localNy, ix);
-        else
-            return globalMdspan()(iy, ix - localNx);
-    }
-
-private:
-    mdspan_t localMdspan(){return mdspan_t(m_local_acc->get_pointer(), localNy, localNx);}
-    const mdspan_t localMdspan() const {return mdspan_t(m_local_acc->get_pointer(), localNy, localNx);}
-
-    mdspan_t globalMdspan(){return mdspan_t(m_global_acc->get_pointer(), globalNy, globalNx);}
-    const mdspan_t globalMdspan() const {return mdspan_t(m_global_acc->get_pointer(), globalNy, globalNx);}
-    // StraddledBuffer(const float percentage_in_local_mem, const size_t ny,
-    //                 const size_t nx, const size_t ny1, sycl::handler &cgh) {
-    // }
-
 
     // ctor(percentage alloc, ny, nx, ny1, cgh_for_local_accessor)
-
-    // index_limit
-    //  operator[i] doing the if i > index_limit return global accessor else
-    //  return local_accessor
 };
 
 // ==========================================
@@ -121,7 +99,7 @@ memory and the second one fills the local accessor and solves the advection?*/
         auto fdist =
             buff_fdistrib.get_access<sycl::access::mode::read_write>(cgh);
 
-         StraddledBuffer<double>::globAcc2D overslice_ftmp =
+        globAcc2D overslice_ftmp =
             buff_rest_nx.get_access<sycl::access::mode::discard_read_write>(
                 cgh);
 
@@ -130,15 +108,15 @@ memory and the second one fills the local accessor and solves the advection?*/
         sycl::local_accessor<double, 2> slice_ftmp(
             sycl::range<2>(wg_size_y, local_malloc_size), cgh, sycl::no_init);
 
-
         cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<3> g) {
             // if y1 > 1 //if we have a stide, we transpose, else we copy
-            mdspan_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
-            StraddledBuffer<double> BUFF(overslice_ftmp, slice_ftmp);
 
             /* Copy kernel*/
             g.parallel_for_work_item(
                 sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
+                    mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
+                    StraddledBuffer<double> BUFF(overslice_ftmp, slice_ftmp);
+                    
                     const int ix = it.get_local_id(1);
                     const int iy1 = g.get_group_id(2);
 
@@ -146,15 +124,18 @@ memory and the second one fills the local accessor and solves the advection?*/
                     const int iy =
                         wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
 
-
                     BUFF(iy, ix) = fdist_view(iy, ix, iy1);
                     // if (ix < MAX_NX_ALLOC) {
                     //     // slice_ftmp[local_ny][ix] = fdist[iy][ix][iy1];
-                    //     (*BUFF.m_local_acc)[local_ny][ix] = fdist_view(iy, ix, iy1);
-                    //     // slice_ftmp[local_ny][ix] = fdist_view(iy, ix, iy1);
+                    //     (*BUFF.m_local_acc)[local_ny][ix] = fdist_view(iy,
+                    //     ix, iy1);
+                    //     // slice_ftmp[local_ny][ix] = fdist_view(iy, ix,
+                    //     iy1);
                     // } else {
-                    //     (*BUFF.m_global_acc)[iy][ix - MAX_NX_ALLOC] = fdist_view(iy, ix, iy1);
-                    //     // overslice_ftmp[iy][ix - MAX_NX_ALLOC] = fdist_view(iy, ix, iy1);
+                    //     (*BUFF.m_global_acc)[iy][ix - MAX_NX_ALLOC] =
+                    //     fdist_view(iy, ix, iy1);
+                    //     // overslice_ftmp[iy][ix - MAX_NX_ALLOC] =
+                    //     fdist_view(iy, ix, iy1);
                     //     // fdist[iy][ix][iy1];
                     // }
                 });   // barrier
@@ -162,6 +143,9 @@ memory and the second one fills the local accessor and solves the advection?*/
             /* Solve kernel */
             g.parallel_for_work_item(
                 sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
+                    mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
+                    StraddledBuffer<double> BUFF(overslice_ftmp, slice_ftmp);
+
                     const int ix = it.get_local_id(1);
                     const int iy1 = g.get_group_id(2);
 
@@ -188,19 +172,21 @@ memory and the second one fills the local accessor and solves the advection?*/
                         int idx_ipos1 = (nx + ipos1 + k) % nx;
 
                         fdist_view(iy, ix, iy1) +=
-                                coef[k] * BUFF(iy, idx_ipos1);
+                            coef[k] * BUFF(iy, idx_ipos1);
 
                         // if (idx_ipos1 < MAX_NX_ALLOC) {
                         //     fdist_view(iy, ix, iy1) +=
-                        //         coef[k] * (*BUFF.m_local_acc)[local_ny][idx_ipos1];
+                        //         coef[k] *
+                        //         (*BUFF.m_local_acc)[local_ny][idx_ipos1];
                         //         // coef[k] * slice_ftmp[local_ny][idx_ipos1];
                         // } else {
                         //     fdist_view(iy, ix, iy1) +=
                         //         coef[k] *
-                        //         (*BUFF.m_global_acc)[iy][idx_ipos1 - MAX_NX_ALLOC];
-                        //         // overslice_ftmp[iy][idx_ipos1 - MAX_NX_ALLOC];
+                        //         (*BUFF.m_global_acc)[iy][idx_ipos1 -
+                        //         MAX_NX_ALLOC];
+                        //         // overslice_ftmp[iy][idx_ipos1 -
+                        //         // // MAX_NX_ALLOC];
                         // }
-
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
         });           // end parallel_for_work_group
@@ -219,11 +205,11 @@ AdvX::Exp1::operator()(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
     auto rest_malloc = nx <= MAX_NX_ALLOC ? 0 : nx - MAX_NX_ALLOC;
 
     // On A100 it breaks when ny (the first dimension) is >= 65536.
-    if (ny < MAX_NY) {
-        /* If limit not exceeded we return a classical Hierarchical advector */
-        AdvX::Hierarchical adv{};
-        return adv(Q, buff_fdistrib, params);
-    } else {
+    // if (ny < MAX_NY) {
+    //     /* If limit not exceeded we return a classical Hierarchical advector */
+    //     AdvX::Hierarchical adv{};
+    //     return adv(Q, buff_fdistrib, params);
+    // } else {
         double div = static_cast<double>(ny) / static_cast<double>(MAX_NY);
         auto floor_div = std::floor(div);
         auto is_int = div == floor_div;
@@ -249,5 +235,5 @@ AdvX::Exp1::operator()(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
         // return the last advection with the rest
         return actual_advection(Q, buff_fdistrib, params, ny_size, ny_offset,
                                 rest_malloc);
-    }
+    // }
 }
