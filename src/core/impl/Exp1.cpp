6707aa35dba9c8e6ba0169963169978d247389ec
Aympab <aymeric.millan@cea.fr>
2024-09-18 10:13:48 +0200

    commit a bug

--- src/core/impl/Exp1.cpp
+++ src/core/impl/Exp1.cpp
@@ -12,18 +12,7 @@ using mdspan2d_t =
     std::experimental::mdspan<real_t, std::experimental::dextents<size_t, 2>,
                               std::experimental::layout_right>;
 
-using globAcc2D =
-    typename sycl::accessor<real_t, 2,
-                            sycl::access::mode::discard_read_write,
-                            sycl::target::global_buffer>;
-
-using localAcc2D = typename sycl::local_accessor<real_t, 2>;
-//TODO: implement percent_in_global_mem values
-
-//constexpr size_t MAX_NX_ALLOC = 6144;   // A100
-
-// constexpr size_t MAX_NX_ALLOC = 6144;
-// constexpr size_t MAX_NY = 65535;
+using localAcc2d_t = sycl::local_accessor<real_t, 2>;
 
 template <typename RealType> struct StraddledBuffer {
 
@@ -31,9 +20,10 @@ template <typename RealType> struct StraddledBuffer {
     mdspan2d_t m_global_mdpsan;
 
     StraddledBuffer() = delete;
-    StraddledBuffer(const localAcc2D &localAcc, RealType *global_ptr,
-                    size_t globalNx, size_t globalNy)
-        : m_local_mdpsan(localAcc.get_pointer(), localAcc.get_range().get(0),
+    StraddledBuffer(const localAcc2d_t &localAcc, RealType *global_ptr,
+                    size_t globalNy, size_t globalNx)
+        : m_local_mdpsan(localAcc.get_pointer(),
+                         localAcc.get_range().get(0),
                          localAcc.get_range().get(1)),
           m_global_mdpsan(global_ptr, globalNy, globalNx) {}
 
@@ -69,6 +59,7 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
     auto const inv_dx = params.inv_dx;
 
     auto const wg_size_y = params.wg_size_y;
+    auto const wg_size_x = params.wg_size_x;
 
     /* ny must be divisible by slice_size_dim_y */
     if (ny_batch_size % wg_size_y != 0) {
@@ -77,7 +68,7 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
     }
 
     const sycl::range nb_wg{ny_batch_size / wg_size_y, 1, ny1};
-    const sycl::range wg_size{params.wg_size_y, params.wg_size_x, 1};
+    const sycl::range wg_size{wg_size_y, wg_size_x, 1};
 
     return Q.submit([&](sycl::handler &cgh) {
         auto fdist =
@@ -96,7 +87,7 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
                 sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
                     mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
                     StraddledBuffer<double> BUFF(
-                        slice_ftmp, this->buffer_rest_nx, this->ny_,
+                        slice_ftmp, this->buffer_rest_nx, ny,
                         this->overslice_nx_size_);
 
                     const int ix = it.get_local_id(1);
@@ -107,19 +98,6 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
                         wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
 
                     BUFF(iy, ix) = fdist_view(iy, ix, iy1);
-                    // if (ix < MAX_NX_ALLOC) {
-                    //     // slice_ftmp[local_ny][ix] = fdist[iy][ix][iy1];
-                    //     (*BUFF.m_local_acc)[local_ny][ix] = fdist_view(iy,
-                    //     ix, iy1);
-                    //     // slice_ftmp[local_ny][ix] = fdist_view(iy, ix,
-                    //     iy1);
-                    // } else {
-                    //     (*BUFF.m_global_acc)[iy][ix - MAX_NX_ALLOC] =
-                    //     fdist_view(iy, ix, iy1);
-                    //     // overslice_ftmp[iy][ix - MAX_NX_ALLOC] =
-                    //     fdist_view(iy, ix, iy1);
-                    //     // fdist[iy][ix][iy1];
-                    // }
                 });   // barrier
 
             /* Solve kernel */
@@ -127,7 +105,7 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
                 sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
                     mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
                     StraddledBuffer<double> BUFF(
-                        slice_ftmp, this->buffer_rest_nx, this->ny_,
+                        slice_ftmp, this->buffer_rest_nx, ny,
                         this->overslice_nx_size_);
 
                     const int ix = it.get_local_id(1);
@@ -157,20 +135,6 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
 
                         fdist_view(iy, ix, iy1) +=
                             coef[k] * BUFF(iy, idx_ipos1);
-
-                        // if (idx_ipos1 < MAX_NX_ALLOC) {
-                        //     fdist_view(iy, ix, iy1) +=
-                        //         coef[k] *
-                        //         (*BUFF.m_local_acc)[local_ny][idx_ipos1];
-                        //         // coef[k] * slice_ftmp[local_ny][idx_ipos1];
-                        // } else {
-                        //     fdist_view(iy, ix, iy1) +=
-                        //         coef[k] *
-                        //         (*BUFF.m_global_acc)[iy][idx_ipos1 -
-                        //         MAX_NX_ALLOC];
-                        //         // overslice_ftmp[iy][idx_ipos1 -
-                        //         // // MAX_NX_ALLOC];
-                        // }
                     }
                 });   // end parallel_for_work_item --> Implicit barrier
         });           // end parallel_for_work_group
@@ -182,10 +146,8 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
 sycl::event
 AdvX::Exp1::operator()(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
                        const ADVParams &params) {
-    // auto const nx = params.nx;
-    // auto const ny = params.ny;
-    // auto const ny1 = params.ny1;
 
+    // reset_buffer(params.ny);
     // On A100 it breaks when ny (the first dimension) is >= 65536.
     // if (ny < MAX_NY) {
     //     /* If limit not exceeded we return a classical Hierarchical advector */
@@ -201,14 +163,14 @@ AdvX::Exp1::operator()(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
         for (size_t i_batch = 0; i_batch < this->n_batch_ - 1; ++i_batch) {
             size_t ny_offset = (i_batch * this->MAX_NY_BATCH);
 
+            // std::cout << "iter " << i_batch << " ny_offset=" <<ny_offset << std::endl;
+
             actual_advection(Q, buff_fdistrib, params, this->MAX_NY_BATCH, ny_offset)
                 .wait();
         }
-
-        // for the last one we take the rest, we add n_batch-1 because we
-        // processed MAX_SIZE-1 each batch
   
         // return the last advection with the rest
+        // std::cout << "last iter " << n_batch_ << " ny_offset=" <<last_batch_offset_ny_ << " ny_size=" << last_batch_size_ny_ << std::endl;
         return actual_advection(Q, buff_fdistrib, params, last_batch_size_ny_,
                                 last_batch_offset_ny_);
         // }
