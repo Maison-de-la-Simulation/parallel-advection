b644d580141b21ef030354b992a3acb0a6125edd
Aympab <aymeric.millan@cea.fr>
2024-09-19 14:00:19 +0200

    fix this in lambda

--- src/core/impl/Exp1.cpp
+++ src/core/impl/Exp1.cpp
@@ -94,64 +94,70 @@ AdvX::Exp1::actual_advection(sycl::queue &Q,
         sycl::local_accessor<double, 2> slice_ftmp(
             sycl::range<2>(wg_size_y, local_malloc_size), cgh, sycl::no_init);
 
-        cgh.parallel_for_work_group(nb_wg, wg_size, [=](sycl::group<3> g) {
-            // if y1 > 1 //if we have a stide, we transpose, else we copy
+        auto ptr_global = global_vertical_buffer_;
+        auto nx_rest_malloc = nx_rest_malloc_;
 
-            /* Copy kernel*/
-            g.parallel_for_work_item(
-                sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
-                    mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
-                    StraddledBuffer<double> BUFF(global_vertical_buffer_, ny,
-                                                 nx_rest_malloc_, slice_ftmp);
+        cgh.parallel_for_work_group(
+            nb_wg, wg_size,
+            [=, ptr = this->global_vertical_buffer_](sycl::group<3> g) {
+                // if y1 > 1 //if we have a stide, we transpose, else we copy
 
-                    const int ix = it.get_local_id(1);
-                    const int iy1 = g.get_group_id(2);
+                /* Copy kernel*/
+                g.parallel_for_work_item(
+                    sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
+                        mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
+                        StraddledBuffer<double> BUFF(
+                            ptr_global, ny, nx_rest_malloc, slice_ftmp);
 
-                    const int local_ny = it.get_local_id(0);
-                    const int iy =
-                        wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
+                        const int ix = it.get_local_id(1);
+                        const int iy1 = g.get_group_id(2);
 
-                    BUFF(iy, ix) = fdist_view(iy, ix, iy1);
-                });   // barrier
+                        const int local_ny = it.get_local_id(0);
+                        const int iy = wg_size_y * g.get_group_id(0) +
+                                       ny_offset + local_ny;
 
-            /* Solve kernel */
-            g.parallel_for_work_item(
-                sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
-                    mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
-                    StraddledBuffer<double> BUFF(global_vertical_buffer_, ny,
-                                                 nx_rest_malloc_, slice_ftmp);
+                        BUFF(iy, ix) = fdist_view(iy, ix, iy1);
+                    });   // barrier
 
-                    const int ix = it.get_local_id(1);
-                    const int iy1 = g.get_group_id(2);
+                /* Solve kernel */
+                g.parallel_for_work_item(
+                    sycl::range{wg_size_y, nx, 1}, [&](sycl::h_item<3> it) {
+                        mdspan3d_t fdist_view(fdist.get_pointer(), ny, nx, ny1);
+                        StraddledBuffer<double> BUFF(
+                            ptr_global, ny, nx_rest_malloc, slice_ftmp);
 
-                    const int local_ny = it.get_local_id(0);
-                    const int iy =
-                        wg_size_y * g.get_group_id(0) + ny_offset + local_ny;
+                        const int ix = it.get_local_id(1);
+                        const int iy1 = g.get_group_id(2);
 
-                    double const xFootCoord = displ(ix, iy, params);
+                        const int local_ny = it.get_local_id(0);
+                        const int iy = wg_size_y * g.get_group_id(0) +
+                                       ny_offset + local_ny;
 
-                    // index of the cell to the left of footCoord
-                    const int leftNode =
-                        sycl::floor((xFootCoord - minRealX) * inv_dx);
+                        double const xFootCoord = displ(ix, iy, params);
 
-                    const double d_prev1 =
-                        LAG_OFFSET +
-                        inv_dx * (xFootCoord - coord(leftNode, minRealX, dx));
+                        // index of the cell to the left of footCoord
+                        const int leftNode =
+                            sycl::floor((xFootCoord - minRealX) * inv_dx);
 
-                    auto coef = lag_basis(d_prev1);
+                        const double d_prev1 =
+                            LAG_OFFSET +
+                            inv_dx *
+                                (xFootCoord - coord(leftNode, minRealX, dx));
 
-                    const int ipos1 = leftNode - LAG_OFFSET;
+                        auto coef = lag_basis(d_prev1);
 
-                    fdist_view(iy, ix, iy1) = 0.;
-                    for (int k = 0; k <= LAG_ORDER; k++) {
-                        int idx_ipos1 = (nx + ipos1 + k) % nx;
+                        const int ipos1 = leftNode - LAG_OFFSET;
 
-                        fdist_view(iy, ix, iy1) +=
-                            coef[k] * BUFF(iy, idx_ipos1);
-                    }
-                });   // end parallel_for_work_item --> Implicit barrier
-        });           // end parallel_for_work_group
-    });               // end Q.submit
+                        fdist_view(iy, ix, iy1) = 0.;
+                        for (int k = 0; k <= LAG_ORDER; k++) {
+                            int idx_ipos1 = (nx + ipos1 + k) % nx;
+
+                            fdist_view(iy, ix, iy1) +=
+                                coef[k] * BUFF(iy, idx_ipos1);
+                        }
+                    });   // end parallel_for_work_item --> Implicit barrier
+            });           // end parallel_for_work_group
+    });                   // end Q.submit
 }   // end actual_advection
 
 // ==========================================
