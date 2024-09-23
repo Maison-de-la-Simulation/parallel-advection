8ae134b375120d8400fae628fd143e98df43e21f
Aympab <aymeric.millan@cea.fr>
2024-09-23 11:25:31 +0200

    add batched validation

--- src/tools/validation.cpp
+++ src/tools/validation.cpp
@@ -1,5 +1,5 @@
 #include "validation.h"
-#include <algorithm>
+#include <hipSYCL/sycl/sycl.hpp>
 #include <iostream>
 // ==========================================
 // ==========================================
@@ -18,6 +18,8 @@ validate_result(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
     for (size_t iy1=0; iy1 < params.ny1; iy1++) {
 
         double errorL1 = 0.0;
+
+        std::vector<double> errorsL1(params.ny1);
         {
 
             sycl::buffer<double> errorl1_buff(&errorL1, 1);
@@ -73,3 +75,84 @@ validate_result(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
 
     return highest_l1;
 }   // end validate_results
+
+// ==========================================
+// ==========================================
+// double
+// validate_result(sycl::queue &Q, sycl::buffer<double, 3> &buff_fdistrib,
+//                 const ADVParams &params, bool do_print) {
+
+//     auto const dx = params.dx;
+//     auto const dvx = params.dvx;
+//     auto const dt = params.dt;
+//     auto const minRealX = params.minRealX;
+//     auto const minRealVx = params.minRealVx;
+//     auto const maxIter = params.maxIter;
+//     auto const ny1 = params.ny1;
+
+//     // Contains all the sum reductions in the ny1 dimension
+//     sycl::buffer<double, 1> buff_sums(sycl::range(params.ny1));
+//     // min at 0 max at 1
+//     sycl::buffer<double, 1> buff_min_max(sycl::range(2));
+
+//     sycl::buffer<double, 3> buff_err(buff_fdistrib.get_range());
+
+//     sycl::nd_range<3> ndr(sycl::range(params.ny, params.nx, params.ny1),
+//                           sycl::range(params.ny, params.nx, 1));
+
+//     Q.submit([&](sycl::handler &cgh) {
+//          sycl::accessor fdist{buff_fdistrib, cgh, sycl::read_only};
+//          sycl::accessor ferr{buff_err, cgh, sycl::read_write,
+//                              sycl::no_init};
+//          sycl::accessor outputValues{buff_sums, cgh, sycl::write_only,
+//                                      sycl::no_init};
+//          sycl::accessor min_max{buff_min_max, cgh, sycl::write_only,
+//                                 sycl::no_init};
+
+//          cgh.parallel_for(ndr, [=](sycl::nd_item<3> itm) {
+//              const size_t iy = itm.get_global_id(0);
+//              const size_t ix = itm.get_global_id(1);
+//              const size_t iy1 = itm.get_group(2);
+
+//              double const x = minRealX + ix * dx;
+//              double const v = minRealVx + iy * dvx;
+//              double const t = maxIter * dt;
+
+//              auto value = sycl::sin(4 * M_PI * (x - v * t));
+//              auto err = sycl::fabs(fdist[iy][ix][iy1] - value);
+//              ferr[iy][ix][iy1] = err;
+
+//              sycl::group_barrier(itm.get_group());
+
+//             //  //  outputValues[iy1] =
+//             //  //  reduce_over_group(itm.get_group(), ferr[iy][ix][iy1],
+//             //  //  sycl::plus<double>());
+//             //  outputValues[iy1] = 10e-16;
+
+//             //  sycl::group_barrier(itm.get_group());
+
+//             //  double *first = outputValues.get_pointer();
+//             //  double *last = first + ny1;
+//             //  double min = joint_reduce(itm.get_group(), first, last,
+//             //                            sycl::minimum<double>());
+//             //  double max = joint_reduce(itm.get_group(), first, last,
+//             //                            sycl::maximum<double>());
+//             //  min_max[0] = min;
+//             //  min_max[1] = max;
+//          });
+//      }).wait();
+
+//     auto highest_l1 = -1;
+//     auto lowest_l1 = -1;
+//     // sycl::host_accessor host_minmax{buff_min_max};
+//     // highest_l1 = host_minmax[1];
+//     // lowest_l1 = host_minmax[0];
+
+//     if (do_print) {
+//         std::cout << "Highest L1 error found: " << highest_l1 << " (lowest is "
+//                   << lowest_l1 << ")\n"
+//                   << std::endl;
+//     }
+
+//     return highest_l1;
+// }   // end validate_results
