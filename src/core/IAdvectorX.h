14905d267ac4abe8e03c29a61074ca8c1c957fe8
Aympab <aymeric.millan@cea.fr>
2024-10-24 15:09:25 +0200

    add solve functor

--- src/core/IAdvectorX.h
+++ src/core/IAdvectorX.h
@@ -2,96 +2,13 @@
 
 #include "AdvectionParams.h"
 #include <sycl/sycl.hpp>
-
-/* Lagrange variables, order, number of points, offset from the current point */
-int static constexpr LAG_ORDER = 5;
-int static constexpr LAG_PTS = 6;
-int static constexpr LAG_OFFSET = 2;
-double static constexpr loc[] = {-1. / 24, 1. / 24.,  -1. / 12.,
-                                 1. / 12., -1. / 24., 1. / 24.};
+#include <Solver.h>
 
 class IAdvectorX {
   public:
     virtual ~IAdvectorX() = default;
 
-    virtual sycl::event operator()(sycl::queue &Q,
-                                   double* fdist_dev,
+    virtual sycl::event operator()(sycl::queue &Q, double *fdist_dev,
                                    const ADVParams &params) = 0;
 
-    // ==========================================
-    // ==========================================
-    /* Computes the real position of x or speed of vx based on discretization */
-    [[nodiscard]] static inline __attribute__((always_inline)) double
-    coord(const int i, const double &minValue,
-          const double &delta) noexcept {
-        return minValue + i * delta;
-    }
-
-    // ==========================================
-    // ==========================================
-    /* Computes the coefficient for semi lagrangian interp of order 5 */
-    [[nodiscard]] static inline __attribute__((always_inline))
-    std::array<double, LAG_PTS>
-    lag_basis(double px) noexcept {
-        std::array<double, LAG_PTS> coef;
-
-        const double pxm2 = px - 2.;
-        const double sqrpxm2 = pxm2 * pxm2;
-        const double pxm2_01 = pxm2 * (pxm2 - 1.);
-
-        coef[0] = loc[0] * pxm2_01 * (pxm2 + 1.) * (pxm2 - 2.) * (pxm2 - 1.);
-        coef[1] = loc[1] * pxm2_01 * (pxm2 - 2.) * (5 * sqrpxm2 + pxm2 - 8.);
-        coef[2] = loc[2] * (pxm2 - 1.) * (pxm2 - 2.) * (pxm2 + 1.) *
-                  (5 * sqrpxm2 - 3 * pxm2 - 6.);
-        coef[3] = loc[3] * pxm2 * (pxm2 + 1.) * (pxm2 - 2.) *
-                  (5 * sqrpxm2 - 7 * pxm2 - 4.);
-        coef[4] =
-            loc[4] * pxm2_01 * (pxm2 + 1.) * (5 * sqrpxm2 - 11 * pxm2 - 2.);
-        coef[5] = loc[5] * pxm2_01 * pxm2 * (pxm2 + 1.) * (pxm2 - 2.);
-
-        return coef;
-    }   // end lag_basis
-
-    // ==========================================
-    // ==========================================
-    /* Computes the covered distance by x during dt. returns the feet coord */
-    [[nodiscard]] static inline __attribute__((always_inline)) double
-    displ(const int i1, const int i0, const ADVParams &params) noexcept {
-        auto const minRealX = params.minRealX;
-        auto const minRealVx = params.minRealVx;
-        auto const dx = params.dx;
-        auto const dvx = params.dvx;
-        auto const dt = params.dt;
-        auto const realWidthX = params.realWidthX;
-
-        double const x = coord(i1, minRealX, dx);
-        double const vx = coord(i0, minRealVx, dvx);
-
-        double const displx = dt * vx;
-
-        return minRealX +
-               sycl::fmod(realWidthX + x - displx - minRealX, realWidthX);
-    }   // end displ
-
-    // ==========================================
-    // ==========================================
-    [[nodiscard]] static inline __attribute__((always_inline)) double
-    displ(const int i1,
-          const int i0,
-          const double minRealX,
-          const double minRealVx,
-          const double realWidthX,
-          const double dx,
-          const double dvx,
-          const double dt
-          ) noexcept {
-
-        double const x = coord(i1, minRealX, dx);
-        double const vx = coord(i0, minRealVx, dvx);
-
-        double const displx = dt * vx;
-
-        return minRealX +
-               sycl::fmod(realWidthX + x - displx - minRealX, realWidthX);
-    }   // end displ
 };
---
+++ src/core/Solver.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "AdvectionParams.h"
+#include <sycl/sycl.hpp>
+
+/* Lagrange variables, order, number of points, offset from the current point */
+int static constexpr LAG_ORDER = 5;
+int static constexpr LAG_PTS = 6;
+int static constexpr LAG_OFFSET = 2;
+double static constexpr loc[] = {-1. / 24, 1. / 24.,  -1. / 12.,
+                                 1. / 12., -1. / 24., 1. / 24.};
+
+// using mdspan1D_stride_t =
+//     std::experimental::mdspan<double, std::experimental::dextents<size_t, 1>,
+//                               std::experimental::layout_stride>;
+
+// auto slice = std::experimental::submdspan(
+//     fdist, i0, std::experimental::full_extent, i2);
+
+struct Solver {
+    ADVParams p;   // mettre que le necessaire
+
+    // ==========================================
+    // ==========================================
+    /* Computes the real position of x or speed of vx based on discretization */
+    [[nodiscard]] static inline __attribute__((always_inline)) double
+    coord(const int i, const double &minValue, const double &delta) noexcept {
+        return minValue + i * delta;
+    }
+
+    // ==========================================
+    // ==========================================
+    /* Computes the coefficient for semi lagrangian interp of order 5 */
+    [[nodiscard]] static inline
+        __attribute__((always_inline)) std::array<double, LAG_PTS>
+        lag_basis(double px) noexcept {
+        std::array<double, LAG_PTS> coef;
+
+        const double pxm2 = px - 2.;
+        const double sqrpxm2 = pxm2 * pxm2;
+        const double pxm2_01 = pxm2 * (pxm2 - 1.);
+
+        coef[0] = loc[0] * pxm2_01 * (pxm2 + 1.) * (pxm2 - 2.) * (pxm2 - 1.);
+        coef[1] = loc[1] * pxm2_01 * (pxm2 - 2.) * (5 * sqrpxm2 + pxm2 - 8.);
+        coef[2] = loc[2] * (pxm2 - 1.) * (pxm2 - 2.) * (pxm2 + 1.) *
+                  (5 * sqrpxm2 - 3 * pxm2 - 6.);
+        coef[3] = loc[3] * pxm2 * (pxm2 + 1.) * (pxm2 - 2.) *
+                  (5 * sqrpxm2 - 7 * pxm2 - 4.);
+        coef[4] =
+            loc[4] * pxm2_01 * (pxm2 + 1.) * (5 * sqrpxm2 - 11 * pxm2 - 2.);
+        coef[5] = loc[5] * pxm2_01 * pxm2 * (pxm2 + 1.) * (pxm2 - 2.);
+
+        return coef;
+    }   // end lag_basis
+
+    // ==========================================
+    // ==========================================
+    /* Computes the covered distance by x during dt. returns the feet coord */
+    [[nodiscard]] inline __attribute__((always_inline)) double
+    displ(const int i1, const int i0) const noexcept {
+        double const x = coord(i1, p.minRealX, p.dx);
+        double const vx = coord(i0, p.minRealVx, p.dvx);
+
+        double const displx = p.dt * vx;
+
+        return p.minRealX +
+               sycl::fmod(p.realWidthX + x - displx - p.minRealX, p.realWidthX);
+    }   // end displ
+
+    // ==========================================
+    // ==========================================
+    /* The _solve_ function of the algorithm presented */
+    template <class ArrayLike1D>
+    double operator()(const size_t &i0, const size_t &i1, const size_t &i2,
+                      ArrayLike1D data) const {
+
+        double const xFootCoord = displ(i1, i0);
+
+        // index of the cell to the left of footCoord
+        const int leftNode = sycl::floor((xFootCoord - p.minRealX) * p.inv_dx);
+
+        const double d_prev1 =
+            LAG_OFFSET +
+            p.inv_dx * (xFootCoord - coord(leftNode, p.minRealX, p.dx));
+
+        auto coef = lag_basis(d_prev1);
+
+        const int ipos1 = leftNode - LAG_OFFSET;
+
+        double value = 0.;
+        for (int k = 0; k <= LAG_ORDER; k++) {
+            int id1_ipos = (p.n1 + ipos1 + k) % p.n1;
+
+            value += coef[k] * data(id1_ipos);
+        }
+
+        return value;
+    }
+};
