46d6450311ad61fa02d2cb33ffdbff0507dfed9c
Aympab <aymeric.millan@cea.fr>
2025-01-16 13:56:01 +0100

    remove old tests

--- tests/impl_unittests.cpp
+++ tests/impl_unittests.cpp
@@ -85,7 +85,7 @@ TEST(Impl, Scoped) {
 }
 
 // =============================================================================
-TEST(Impl, StraddledMalloc) {
+TEST(Impl, Exp1) {
     ADVParams params;
     sycl::queue Q;
     Solver solver(params);
@@ -94,7 +94,7 @@ TEST(Impl, StraddledMalloc) {
         sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
     fill_buffer(Q, fdist, params);
 
-    auto advector = sref::make_unique<AdvX::StraddledMalloc>();
+    auto advector = sref::make_unique<AdvX::Exp1>(params, Q);
 
     for (size_t i = 0; i < params.maxIter; ++i)
         advector(Q, fdist, solver).wait_and_throw();
@@ -104,7 +104,7 @@ TEST(Impl, StraddledMalloc) {
 }
 
 // =============================================================================
-TEST(Impl, StreamY) {
+TEST(Impl, Exp2) {
     ADVParams params;
     sycl::queue Q;
     Solver solver(params);
@@ -113,7 +113,7 @@ TEST(Impl, StreamY) {
         sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
     fill_buffer(Q, fdist, params);
 
-    auto advector = sref::make_unique<AdvX::StreamY>();
+    auto advector = sref::make_unique<AdvX::Exp2>(params, params.percent_loc, Q);
 
     for (size_t i = 0; i < params.maxIter; ++i)
         advector(Q, fdist, solver).wait_and_throw();
@@ -121,6 +121,139 @@ TEST(Impl, StreamY) {
     auto err = validate_result(Q, fdist, params);
     ASSERT_NEAR(err, 0, EPS);
 }
+
+// =============================================================================
+TEST(Impl, Exp3) {
+    ADVParams params;
+    sycl::queue Q;
+    Solver solver(params);
+
+    double *fdist =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+    fill_buffer(Q, fdist, params);
+
+    auto advector = sref::make_unique<AdvX::Exp3>(params, Q);
+
+    for (size_t i = 0; i < params.maxIter; ++i)
+        advector(Q, fdist, solver).wait_and_throw();
+
+    auto err = validate_result(Q, fdist, params);
+    ASSERT_NEAR(err, 0, EPS);
+}
+
+// =============================================================================
+TEST(Impl, Exp4) {
+    ADVParams params;
+    sycl::queue Q;
+    Solver solver(params);
+
+    double *fdist =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+    fill_buffer(Q, fdist, params);
+
+    auto advector = sref::make_unique<AdvX::Exp4>(params);
+
+    for (size_t i = 0; i < params.maxIter; ++i)
+        advector(Q, fdist, solver).wait_and_throw();
+
+    auto err = validate_result(Q, fdist, params);
+    ASSERT_NEAR(err, 0, EPS);
+}
+
+// =============================================================================
+TEST(Impl, Exp5) {
+    ADVParams params;
+    sycl::queue Q;
+    Solver solver(params);
+
+    double *fdist =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+    fill_buffer(Q, fdist, params);
+
+    auto advector = sref::make_unique<AdvX::Exp5>(params);
+
+    for (size_t i = 0; i < params.maxIter; ++i)
+        advector(Q, fdist, solver).wait_and_throw();
+
+    auto err = validate_result(Q, fdist, params);
+    ASSERT_NEAR(err, 0, EPS);
+}
+
+// =============================================================================
+TEST(Impl, Exp6) {
+    ADVParams params;
+    sycl::queue Q;
+    Solver solver(params);
+
+    double *fdist =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+    fill_buffer(Q, fdist, params);
+
+    auto advector = sref::make_unique<AdvX::Exp6>(params, Q);
+
+    for (size_t i = 0; i < params.maxIter; ++i)
+        advector(Q, fdist, solver).wait_and_throw();
+
+    auto err = validate_result(Q, fdist, params);
+    ASSERT_NEAR(err, 0, EPS);
+}
+
+// =============================================================================
+TEST(Impl, Exp7) {
+    ADVParams params;
+    sycl::queue Q;
+    Solver solver(params);
+
+    double *fdist =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+    fill_buffer(Q, fdist, params);
+
+    auto advector = sref::make_unique<AdvX::Exp7>(params, Q);
+
+    for (size_t i = 0; i < params.maxIter; ++i)
+        advector(Q, fdist, solver).wait_and_throw();
+
+    auto err = validate_result(Q, fdist, params);
+    ASSERT_NEAR(err, 0, EPS);
+}
+
+// =============================================================================
+// TEST(Impl, StraddledMalloc) {
+//     ADVParams params;
+//     sycl::queue Q;
+//     Solver solver(params);
+
+//     double *fdist =
+//         sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+//     fill_buffer(Q, fdist, params);
+
+//     auto advector = sref::make_unique<AdvX::StraddledMalloc>();
+
+//     for (size_t i = 0; i < params.maxIter; ++i)
+//         advector(Q, fdist, solver).wait_and_throw();
+
+//     auto err = validate_result(Q, fdist, params);
+//     ASSERT_NEAR(err, 0, EPS);
+// }
+
+// =============================================================================
+// TEST(Impl, StreamY) {
+//     ADVParams params;
+//     sycl::queue Q;
+//     Solver solver(params);
+
+//     double *fdist =
+//         sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+//     fill_buffer(Q, fdist, params);
+
+//     auto advector = sref::make_unique<AdvX::StreamY>();
+
+//     for (size_t i = 0; i < params.maxIter; ++i)
+//         advector(Q, fdist, solver).wait_and_throw();
+
+//     auto err = validate_result(Q, fdist, params);
+//     ASSERT_NEAR(err, 0, EPS);
+// }
 
 // =============================================================================
 // TEST(Impl, Exp1) {
