4b7b1be753f5ef0fc5f7439e008cc89cb0b1c902
Aympab <aymeric.millan@cea.fr>
2025-03-02 21:46:12 +0100

    update benchmark

--- benchmark/bench_utils.h
+++ benchmark/bench_utils.h
@@ -3,6 +3,7 @@
 #include <AdvectionParams.h>
 #include <advectors.h>
 #include <benchmark/benchmark.h>
+#include <cstdint>
 #include <init.h>
 #include <sycl/sycl.hpp>
 
@@ -10,79 +11,84 @@ enum AdvImpl : int {
     BR3D,           // 0
     HIER,           // 1
     NDRA,           // 2
-    SCOP,           // 3
-    STREAMY,        // 4
-    STRAD,          // 5
-    REVIDX,         // 6
-    TWODIMWG,       // 7
-    SEQ_TWODIMWG,   // 8
-    EXP1,           // 9
-    EXP2,           // 10
-    EXP3,           // 11
-    EXP4,           // 12
-    EXP5,           // 13
-    EXP6,           // 14
-    // HybridMem,     // 15
-    FULLYGLOBAL,   // 16
-    FULLYLOCAL,    // 17
+    ADAPTWG,        // 3
+    HYBRID          // 4
 };
 
 using bm_vec_t = std::vector<int64_t>;
-static bm_vec_t N0_LARGE_RANGE = benchmark::CreateRange(2 << 5, 2 << 20, 2);
-static bm_vec_t N0_RANGE = {16384, 32768, 65535};
-static bm_vec_t N2_RANGE = {1, 2, 4, 8, 16, 32, 64};
-static int64_t n1 = 1024;
 
-static bm_vec_t WG_SIZES_X_RANGE = {1, 4, 8, 64, 128, 256, 512, 1024};
+static bm_vec_t N0_RANGE = {8192};
+static bm_vec_t N1_RANGE = {16, 1024, 6100};
+static bm_vec_t N2_RANGE = {128};
+static bm_vec_t PERCENT_LOC = {1, 10, 50, 80, 99};
+static bm_vec_t SEQ_SIZE0 = {1};
+static bm_vec_t SEQ_SIZE2 = {1};
 
-static bm_vec_t IMPL_RANGE = {
-    // AdvImpl::BR3D,
-    AdvImpl::HIER,     AdvImpl::NDRA,         AdvImpl::SCOP,
-    AdvImpl::STREAMY,  AdvImpl::STRAD,        AdvImpl::REVIDX,
-    AdvImpl::TWODIMWG, AdvImpl::SEQ_TWODIMWG,
-    AdvImpl::EXP1, AdvImpl::EXP2, AdvImpl::EXP3, AdvImpl::EXP4,
-    AdvImpl::EXP5, AdvImpl::EXP6, /*AdvImpl::HybridMem*/
-    AdvImpl::FULLYGLOBAL, AdvImpl::FULLYLOCAL,
-    };
+static std::vector<bm_vec_t> EXP_RANGE{
+    {1<<17, 1<<14, 1},     //n1 trop grand pour local mem, acces coal
+    {1<<10, 1<<11, 1<<10}, //n1 trop grand (WI per WG) mais rentre en local mem
+    {1<<10, 1<<14, 1<<7},  //n1 trop grand pour fit en local +acces non coal.
+    {1<<27, 1<<4,  1},     //n1 trop petit, acces coalescent en dim 1
+    {1<<20, 1<<4,  1<<7},  //n1 trop petit et acces non coalescent
+    {1<<21, 1<<10, 1},     //cas parfait: elements contigus
+    {1<<14, 1<<10, 1<<7},  //elements espacés en memoire de plus de SIMD_Size
+    // {0,1<<10,1<<6+1},   //non aligné et pas power of two
+    {1    , 1<<10, 1<<21}, //un seul batch en d0
+    {1<<11, 1<<10, 1<<10}, //profil equilibre
+};
 
-// static bm_vec_t IMPL_RANGE_NO_SCOPED = {
-//                               AdvImpl::BR3D, AdvImpl::HIER, AdvImpl::NDRA,
-//                               AdvImpl::STREAMY, AdvImpl::STRAD,
-//                               AdvImpl::REVIDX, AdvImpl::TWODIMWG,
-//                               AdvImpl::SEQ_TWODIMWG};
+static int64_t WG_SIZE_NVI = 128;
+static int64_t WG_SIZE_AMD = 256;
+static int64_t WG_SIZE_PVC = 1024;
+
+static bm_vec_t WG_SIZES_RANGE = {1, 4, 8, 64, 128, 256, 512, 1024};
+
+static bm_vec_t IMPL_RANGE = {AdvImpl::BR3D, AdvImpl::HIER, AdvImpl::NDRA,
+                              AdvImpl::ADAPTWG, AdvImpl::HYBRID};
+
 
 // =============================================
 // =============================================
-[[nodiscard]] inline ADVParams
-createParams(const bool gpu, const size_t &n0, const size_t &n1,
-             const size_t &n2) {
-    ADVParams p;
+struct BenchParams {
+    int64_t gpu, n0, n1, n2, w, percent_loc, s0, s2;
 
-    p.outputSolution = false;
-    p.loc_wg_size_0 = 1;
-    p.loc_wg_size_1 = 128;
-    p.loc_wg_size_2 = 1;
+    ADVParams adv_params;
 
-    p.glob_wg_size_0 = 1;
-    p.glob_wg_size_1 = 128;
-    p.glob_wg_size_2 = 1;
+    BenchParams() = delete;
+    BenchParams(benchmark::State &state)
+        : gpu(state.range(0)), n0(state.range(1)), n1(state.range(2)),
+          n2(state.range(3)), w(state.range(4)), percent_loc(state.range(5)),
+          s0(state.range(6)), s2(state.range(7)) {
+        
+            init_params();
+          }
 
-    /* Static physicals params*/
-    p.dt = 0.001;
-    p.minRealX = 0;
-    p.maxRealX = 1;
-    p.minRealVx = -1;
-    p.maxRealVx = 1;
+    void init_params(){
+        auto &p = adv_params;
 
-    /* Dynamic benchmark params*/
-    p.gpu = gpu;
-    p.n1 = n1;
-    p.n0 = n0;
-    p.n2 = n2;
+        p.outputSolution = false;
+        p.pref_wg_size = w;
 
-    p.update_deltas();
-    return p;
-}
+        /* Static physicals params*/
+        p.dt = 0.001;
+        p.minRealX = 0;
+        p.maxRealX = 1;
+        p.minRealVx = -1;
+        p.maxRealVx = 1;
+
+        /* Dynamic benchmark params*/
+        p.gpu = gpu;
+        p.n1 = n1;
+        p.n0 = n0;
+        p.n2 = n2;
+
+        p.percent_loc = static_cast<float>(percent_loc/100.0);
+        p.seq_size0 = s0;
+        p.seq_size2 = s2;
+
+        p.update_deltas();
+    }
+};
 
 // =============================================
 // =============================================
@@ -119,50 +125,11 @@ advectorFactory(const sycl::queue &q, ADVParams &p, Solver &s,
     case AdvImpl::NDRA:
         params.kernelImpl = "NDRange";
         break;
-    case AdvImpl::SCOP:
-        params.kernelImpl = "Scoped";
+    case AdvImpl::ADAPTWG:
+        params.kernelImpl = "AdaptiveWg";
         break;
-    case AdvImpl::STREAMY:
-        params.kernelImpl = "StreamY";
-        break;
-    case AdvImpl::STRAD:
-        params.kernelImpl = "StraddledMalloc";
-        break;
-    case AdvImpl::REVIDX:
-        params.kernelImpl = "ReverseIndexes";
-        break;
-    case AdvImpl::TWODIMWG:
-        params.kernelImpl = "TwoDimWG";
-        break;
-    case AdvImpl::SEQ_TWODIMWG:
-        params.kernelImpl = "SeqTwoDimWG";
-        break;
-    case AdvImpl::EXP1:
-        params.kernelImpl = "Exp1";
-        break;
-    case AdvImpl::EXP2:
-        params.kernelImpl = "Exp2";
-        break;
-    case AdvImpl::EXP3:
-        params.kernelImpl = "Exp3";
-        break;
-    case AdvImpl::EXP4:
-        params.kernelImpl = "Exp4";
-        break;
-    case AdvImpl::EXP5:
-        params.kernelImpl = "Exp5";
-        break;
-    case AdvImpl::EXP6:
-        params.kernelImpl = "Exp6";
-        break;
-    // case AdvImpl::HybridMem:
-    //     params.kernelImpl = "HybridMem";
-    //     break;
-    case AdvImpl::FULLYGLOBAL:
-        params.kernelImpl = "FullyGlobal";
-        break;
-    case AdvImpl::FULLYLOCAL:
-        params.kernelImpl = "FullyLocal";
+    case AdvImpl::HYBRID:
+        params.kernelImpl = "HybridMem";
         break;
     default:
         auto str = "Error: wrong kernel_id.\n";
---
+++ benchmark/bkm_bench.cpp
@@ -0,0 +1,84 @@
+#include "bench_utils.h"
+#include <benchmark/benchmark.h>
+#include <validation.h>
+
+
+// ==========================================
+// ==========================================
+/* Benchmark the impact of wg_size on Hierarchical kernel */
+static void
+BM_HybridMem_p(benchmark::State &state) {
+
+    BenchParams bench_params(state);
+    auto& params = bench_params.adv_params;
+
+    /* SYCL setup */
+    auto Q = createSyclQueue(params.gpu, state);
+    auto data =
+        sycl::malloc_device<double>(params.n0 * params.n1 * params.n2, Q);
+
+    /* Advector setup */
+    Solver solver(params);
+    const auto kernel_id = AdvImpl::HYBRID;
+    const auto advector = advectorFactory(Q, params, solver, kernel_id, state);
+
+    /* Benchmark infos */
+    state.counters.insert({
+        {"gpu", params.gpu},
+        {"n0", params.n0},
+        {"n1", params.n1},
+        {"n2", params.n2},
+        {"kernel_id", kernel_id},
+        {"pref_wg_size", params.pref_wg_size},
+        {"percent_loc", params.percent_loc},
+        {"seq_size0", params.seq_size0},
+        {"seq_size2", params.seq_size2},
+    });
+
+    /* Physics setup */
+    fill_buffer(Q, data, params);
+    Q.wait();
+
+    /* Benchmark */
+    for (auto _ : state) {
+        try {
+            advector(Q, data, solver);
+            Q.wait();
+        } catch (const sycl::exception &e) {
+            state.SkipWithError(e.what());
+            break;
+        }
+    }
+
+    params.maxIter = state.iterations();
+
+    state.SetItemsProcessed(params.maxIter * params.n0 * params.n1 * params.n2);
+    state.SetBytesProcessed(params.maxIter * params.n0 * params.n1 * params.n2 *
+                            sizeof(double));
+
+    auto err = validate_result(Q, data, params, false);
+    if (err > 10e-6) {
+        state.SkipWithError("Validation failed with numerical error > 10e-6.");
+    }
+
+    sycl::free(data, Q);
+}
+
+// ==========================================
+BENCHMARK(BM_HybridMem_p)
+    ->ArgsProduct({
+        {1},             /*gpu*/
+        N0_RANGE,        /*n0*/
+        N1_RANGE,            /*n1*/
+        N2_RANGE,        /*n2*/
+        {WG_SIZE_NVI, 512},         /*w*/
+        PERCENT_LOC,
+        SEQ_SIZE0,
+        SEQ_SIZE2,
+    })
+    ->UseRealTime()
+    ->Unit(benchmark::kMillisecond);
+
+// ==========================================
+// ==========================================
+BENCHMARK_MAIN();
