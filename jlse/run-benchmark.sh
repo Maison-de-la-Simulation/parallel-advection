e99c858794bfa4de572a19181ee868ec1a2ec9bb
Aympab <aympab@gmail.com>
2025-03-31 14:48:33 +0000

    update benchmark script

--- jlse/run-benchmark.sh
+++ jlse/run-benchmark.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-export OUTFILE="/home/ac.amillan/source/parallel-advection/jlse/out/pvc/acpp.json"
+export HW=pvc
+export SYCL=acpp
+export APP=conv1d
 export REP=10
-# export BENCHMARK_FILTER=BM_Advector/0
 
-export EXEC="/home/ac.amillan/source/parallel-advection/build_dpcpp_pvc/benchmark/paper-bench"
+export OUTFILE="/home/ac.amillan/source/parallel-advection/jlse/out/$APP/$HW/$SYCL.json"
+export EXEC="/home/ac.amillan/source/parallel-advection/build_${SYCL}_${HW}/benchmark/$APP-bench"
 
+# --benchmark_report_aggregates_only=true \
 $EXEC --benchmark_counters_tabular=true \
-      --benchmark_report_aggregates_only=true \
       --benchmark_min_warmup_time=1 \
       --benchmark_format=json \
       --benchmark_repetitions=$REP \
